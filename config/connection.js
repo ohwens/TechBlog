669386e10828c5a47825f1138e164f31018eac36
Ryan Owens <rtowens11@gmail.com>
2021-09-13 14:41:19 -0500

    fixed i hope

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -12,7 +13,9 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-    },
+      port: 3306
+    }
   );
 }
+
+module.exports = sequelize;
